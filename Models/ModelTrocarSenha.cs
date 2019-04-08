e8719eb81db18318607236caad3fbf0c25721d37
Maurício <mauricio@mauricio.gg>
2019-04-08 16:24:56 -0300

    Esqueci de commitar isso

---
+++ Models/ModelTrocarSenha.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoWeb.Models
+{
+	public class ModelTrocarSenha
+	{
+		public string Email { get; set; }
+		public string SenhaAntiga { get; set; }
+		public string SenhaNova { get; set; }
+	}
+}
